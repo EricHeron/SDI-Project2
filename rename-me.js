419de9cf3564f8a23c782d45e5b8c7789fdff564
Eric Heron <echeron@fullsail.edu>
2014-09-10 15:46:24 -0400

    edited the story line to flow better. I also added in the string function and concatenation with String return

--- rename-me.js
+++ rename-me.js
@@ -19,22 +19,18 @@ var myPrompt = prompt("Ready to begin?", "Type yes or no in lowercase");
 if (myPrompt === "yes") {
 	console.log("Let me tell you a little story about " + worker2 + " and " + worker1);
 	console.log(worker1 + " and " + worker2 + " are two young newly weds.");
-	console.log("They have to complete " + numberOfChores + " chores daily.");
+	console.log("They have to complete " + numberOfChores + " chores.");
 	
 	
 // Confirm
 	var myConfirm = confirm("Would you like to continue the story?");
 	if (myConfirm === true) {
-		console.log("Good! They work together to get all the chores done.");
+		console.log("Glad you chose to hear more of the story! They work together to get all the chores done.");
 
 // Boolean Function
 		var choreInfo = function(nameOfChore, timeItTakes) {
 			console.log(nameOfChore + " takes " + timeItTakes + " minutes to complete.");
-			var cleanUp = function(trash) {
-			cleanUp("materials used");
-				console.log(trash + " are picked up");
-			}
-		} 
+		}
 		choreInfo("Laundry", 10);
 // Initiator of While loop
 		var laundry = 10;
@@ -45,6 +41,15 @@ if (myPrompt === "yes") {
 // Return Boolean
 		console.log("All done!");
 		
+// String Function
+		choreInfo("Kitchen", "1 hour and 30");
+		choreInfo("Living Room", "1 hour and 15");
+		var bedroom = function(subRoom1, subRoom2) {
+			console.log(subRoom1 + " clean " + subRoom2 + " clean ");
+		}
+		console.log("All done with the bedroom! It only took an hour.");
+		bedroom("Master bathroom", "and bedroom closet");
+
 	}
 	else {
 		console.log("Refresh the page to start over");
