c4c60dceb71d097ca0c0ae357f99f95f450658a6
Eric Heron <echeron@fullsail.edu>
2014-09-10 11:08:59 -0400

    added confirm

--- rename-me.js
+++ rename-me.js
@@ -14,10 +14,24 @@ var myPrompt;
 
 // Prompt
 var myPrompt = prompt("Ready to begin?", "Type yes or no in lowercase");
+
+// Procedure
 if (myPrompt === "yes") {
 	console.log("Let me tell you a little story about " + worker2 + " and " + worker1);
+	console.log(worker1 + " and " + worker2 + " are two young newly weds.");
+	console.log("They have to complete " + numberOfChores + " chores daily");
+	
+// Confirm
+	var myConfirm = confirm("Would you like to continue the story?");
+	if (myConfirm === true) {
+		console.log("Good! They have 3 amazing pets to take care of together.");
+	}
+	else {
+		console.log("Refresh the page to start over");
+	}
 }
 else {
 	console.log("Thanks for stopping by.");
 };
+
 
