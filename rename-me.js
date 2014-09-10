7f72d76f6dbf70fa05d04b23dca1df54c2917495
Eric Heron <echeron@fullsail.edu>
2014-09-10 14:55:06 -0400

    added boolean function and first while loop

--- rename-me.js
+++ rename-me.js
@@ -19,12 +19,30 @@ var myPrompt = prompt("Ready to begin?", "Type yes or no in lowercase");
 if (myPrompt === "yes") {
 	console.log("Let me tell you a little story about " + worker2 + " and " + worker1);
 	console.log(worker1 + " and " + worker2 + " are two young newly weds.");
-	console.log("They have to complete " + numberOfChores + " chores daily");
+	console.log("They have to complete " + numberOfChores + " chores daily.");
+	
 	
 // Confirm
 	var myConfirm = confirm("Would you like to continue the story?");
 	if (myConfirm === true) {
 		console.log("Good! They have 3 amazing pets to take care of together.");
+
+// Boolean Function
+		var choreInfo = function(nameOfChore, timeItTakes) {
+			console.log(nameOfChore + " takes " + timeItTakes + " minutes to complete.");
+			var cleanUp = function(trash) {
+			cleanUp("materials used");
+				console.log(trash + " are picked up");
+			}
+		} 
+		choreInfo("Laundry", 10);
+// Initiator of While loop
+		var laundry = 10;
+		while (laundry > 0){
+			console.log(laundry + " minutes to go.");
+			laundry-= 5;
+		}
+		console.log("All done!");
 	}
 	else {
 		console.log("Refresh the page to start over");
