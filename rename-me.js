7e3920e671cb7d4e6e169d4291fba6d964e4c1c6
Eric Heron <echeron@fullsail.edu>
2014-09-10 10:44:10 -0400

    added prompt

--- rename-me.js
+++ rename-me.js
@@ -1,5 +1,23 @@
+// Eric Heron
+// SDI 1409
+// Project 2
+// Story about chores
+
+
+
+// Variables
 var worker1 = "Eric";
 var worker2 = "Jennifer";
-var numberOfChores = 5
-var completedChores = true
+var numberOfChores = 5;
+var completedChores = true;
+var myPrompt;
+
+// Prompt
+var myPrompt = prompt("Ready to begin?", "Type yes or no in lowercase");
+if (myPrompt === "yes") {
+	console.log("Let me tell you a little story about " + worker2 + " and " + worker1);
+}
+else {
+	console.log("Thanks for stopping by.");
+};
 
